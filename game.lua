fd0b535d704a3f7a0c451d05726cf2e3600e8fbb
jtellez86 <jtellez86@gmail.com>
2012-10-08 16:48:03 -0500

    Added drag feature to paddle

--- game.lua
+++ game.lua
@@ -4,6 +4,7 @@ local ceiling, leftWall, rightWall, floor
 local background = display.newImage("assets/background.png")
 local paddle = require("paddle")
 local ball = require("ball")
+local hasStarted = false
 
 function setupGame()
 	
@@ -19,16 +20,52 @@ function setupGame()
 	setupWalls()
 	
 	background:addEventListener("touch", background)
+	paddle:addEventListener("touch", dragPaddle)
 	
 	return gameDisplay
 end
 
 
 function background:touch(event)
-	if(event.phase == "began") then 	
+	if(hasStarted == false) then
+		if(event.phase == "began") then 	
 		ball:applyForce(0, -.75, ball.x, ball.y)
+		hasStarted = true
+		end
 	end
+	return true
+end
 
+function dragPaddle( event )
+        local target = event.target
+
+ 
+        local phase = event.phase
+        if "began" == phase then
+              
+			display.getCurrentStage():setFocus( target )
+ 
+                -- Spurious events can be sent to the target, e.g. the user presses 
+                -- elsewhere on the screen and then moves the finger over the target.
+                -- To prevent this, we add this flag. Only when it's true will "move"
+                -- events be sent to the target.
+                target.isFocus = true
+ 
+                -- Store initial position
+                target.x0 = event.x - target.x
+                
+        elseif target.isFocus then
+                if "moved" == phase then
+                        -- Make object move (we subtract t.x0,t.y0 so that moves are
+                        -- relative to initial grab point, rather than object "snapping").
+                        target.x = event.x - target.x0
+                        
+                elseif "ended" == phase or "cancelled" == phase then
+                        display.getCurrentStage():setFocus( nil )
+                        target.isFocus = false
+                end
+        end
+        return true
 end
 
 function addObjectsToDisplay(gameDisplay)
